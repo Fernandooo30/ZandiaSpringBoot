dd2ea74b3218b8b74787b61dd1a776ee1728187d
Angel <alecarnaque96@gmail.com>
2024-05-07 20:32:45 -0500

    primer commit

---
+++ src/test/java/com/example/pizandia/PiZandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiZandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
